0c83f371afbd7ef0e065b64a4c6c788cdd0de3d0
LeonardAdemi <leonardademi126@gmail.com>
2022-02-23 17:06:34 +0100

    commit

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
 	
 	<div class="bar">
 		<div id="mm">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			<p>Lorem ipsum dolor sit LEO, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
